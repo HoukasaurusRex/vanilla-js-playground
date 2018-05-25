d51c288bf8a89516108e2b50672fd3b588939e81
jt1992@gmail.com <jt1992@gmail.com>
2018-05-25 08:41:04 +0800

    Added secret code function to home page

--- public/js/vanilla.js
+++ public/js/vanilla.js
@@ -1,6 +1,19 @@
 function homeCSS() {
   const html = document.documentElement;
+  const pressed = [];
+  const secretCode = 'hellohal';
   html.style.setProperty("--main-background", "url(/assets/backgrounds/vanilla.jpg)");
+  
+  window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    console.log(pressed);
+    if (pressed.join('').includes(secretCode)) {
+      alert('Hello, Dave');
+      cornify_add();
+    }
+  });
 }
 
 /*
